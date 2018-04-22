1c21bf683fd899ef667ffa5f37151163c2c1d359
TOKdawn <dawninshadow0.0@gmail.com>
2018-04-22 19:17:14 +0800

    分镜

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
+var Maxscroll = $("html").height() - $(window).height();
+console.log(Maxscroll);
 window.onscroll = function(e) {
     var Dawn = document.documentElement.scrollTop || document.body.scrollTop;
+
+
     console.log(Dawn)
 }
