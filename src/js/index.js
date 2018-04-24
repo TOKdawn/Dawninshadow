06dd7e5fad2c53ce6fa73761cc3c77b961d4b8c5
TOKdawn <dawninshadow0.0@gmail.com>
2018-04-24 19:55:19 +0800

    时间轴

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,16 @@
-var Maxscroll = $("html").height() - $(window).height();
-console.log(Maxscroll);
-window.onscroll = function(e) {
-    var Dawn = document.documentElement.scrollTop || document.body.scrollTop;
+$(document).ready(function() {
+    $('#Ds_fill').height($(window).height());
+    $(window).resize(function() { //当浏览器大小变化时
+        $('#Ds_fill').height($(window).height());
+        //填充末尾div,以保持scrollTop在各种高度浏览器间恒定
+    });
+    // var Maxscroll = $("html").height() - $(window).height();
+    window.onscroll = function(e) {
+        var Dawn = document.documentElement.scrollTop || document.body.scrollTop;
 
 
-    console.log(Dawn)
-}
+        console.log(Dawn)
+    }
+
+
+});
