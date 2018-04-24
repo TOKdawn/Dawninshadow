362f4110fa2f542e4987d13e88ccfd95c5517889
TOKdawn <dawninshadow0.0@gmail.com>
2018-04-24 22:31:08 +0800

    ..

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
-    $('#Ds_fill').height($(window).height());
+    $('#DS_fill').height($(window).height());
     $(window).resize(function() { //当浏览器大小变化时
-        $('#Ds_fill').height($(window).height());
+        $('#DS_fill').height($(window).height());
         //填充末尾div,以保持scrollTop在各种高度浏览器间恒定
     });
     // var Maxscroll = $("html").height() - $(window).height();
-    window.onscroll = function(e) {
-        var Dawn = document.documentElement.scrollTop || document.body.scrollTop;
-
-
-        console.log(Dawn)
-    }
-
 
 });
+window.onscroll = function(e) {
+    var Dawn = document.documentElement.scrollTop || document.body.scrollTop;
+
+}
