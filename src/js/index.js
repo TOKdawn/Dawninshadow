033102da6074babd46052a61f4875874aab3b29b
TOKdawn <dawninshadow0.0@gmail.com>
2018-04-25 20:22:01 +0800

    requestanimationframe 了解一下

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,54 @@
-$(document).ready(function() {
-    $('#DS_fill').height($(window).height());
-    $(window).resize(function() { //当浏览器大小变化时
-        $('#DS_fill').height($(window).height());
-        //填充末尾div,以保持scrollTop在各种高度浏览器间恒定
-    });
-    // var Maxscroll = $("html").height() - $(window).height();
+var title1 = [` Hello!&nbsp;&nbsp; I'm&nbspTOKdawn`,
+    `&nbsp;ello!&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;llo!&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;lo!&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;o!&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'m&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;m&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbspOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbspKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;awn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;wn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;wn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;n`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp`,
+]
 
-});
+var Change1 = function(type, id) {
+    var opacityNum = 'translateY(-' + type + '%)';
+    var scaleY = 'scaleY(' + (1 + (type / 3)) + ')';
+    var bgcolor = ' hsl(0, 0% ,' + (100 - (type * 2)) + '%)';
+    var textlimut = Math.ceil(type / 3);
+
+    // console.log(opacityNum);
+    $('#' + id).css('transform', opacityNum);
+    $('#DS_line').css({ 'transform': scaleY, 'background-color': bgcolor });
+
+    $("#DS_title_one").html(title1[textlimut]);
+}
+
 window.onscroll = function(e) {
     var Dawn = document.documentElement.scrollTop || document.body.scrollTop;
+    // console.log(Dawn);
+    var Progress;
+    if (Dawn >= 0 && Dawn < 5) {
+        $('#Ds_watch_more').css('display', 'block');
+    } else {
+        $('#Ds_watch_more').css('display', 'none');
+    }
+    if (Dawn > 0 && Dawn < 500) {
+        $('#DS_div_one').addClass("active");
+        Progress = parseInt((Dawn - 0) / 5);
+        console.log('%' + Progress);
+        Change1(Progress, "DS_div_one");
+    }
+    if (Dawn > 500 && Dawn < 1000) {
+        $('#DS_div_one').removeClass("active");
+
+    }
 
 }
