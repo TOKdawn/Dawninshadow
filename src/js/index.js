be0ee4385013c7bbcd851c04def15143229d4281
TOKdawn <dawninshadow0.0@gmail.com>
2018-04-26 13:41:36 +0800

    首页完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,42 @@
-var title1 = [` Hello!&nbsp;&nbsp; I'm&nbspTOKdawn`,
-    `&nbsp;ello!&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
-    `&nbsp;&nbsp;llo!&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
-    `&nbsp;&nbsp;&nbsp;lo!&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;o!&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; I'm&nbsp;TOKdawn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'m&nbsp;TOKdawn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;m&nbsp;TOKdawn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TOKdawn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbspOKdawn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbspKdawn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dawn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;awn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;wn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;wn`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;n`,
-    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp`,
-]
+var title1 = [` Hello!&nbsp;<br> I'm&nbspTOKdawn`,
+    `&nbsp;ello!&nbsp;<br> I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;llo!&nbsp;<br> I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;lo!&nbsp;<br> I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;o!&nbsp;<br> I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;!&nbsp;<br> I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>I'm&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;'m&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;m&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;TOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbspOKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbspKdawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dawn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;awn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;wn`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;n`,
+    `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`,
 
+]
+var describe = [
+    '我是前端开发者?普通大学生?<br>口琴爱好者?',
+    '也是前端开发者?普通大学生?<br>口琴爱好者?',
+    '也是求知开发者.普通大学生?<br>口琴爱好者?',
+    '也是求知开发者.普通大学生?<br>口琴爱好者?',
+    '也是求知的弱鸡.普通大学生?<br>口琴爱好者?',
+    '也是求知的弱鸡.普通大学生?<br>口琴爱好者?',
+    '也是求知的弱鸡.普通大学生?<br>口琴爱好者?',
+    '也是求知的弱鸡?中二大学生?<br>口琴爱好者?',
+    '也是求知的弱鸡?中二大学生?<br>口琴爱好者?',
+    '也是求知的弱鸡?中二病患者?<br>口琴爱好者?',
+    '也是求知的弱鸡?中二病患者?<br>口琴爱好者?',
+    '也是求知的弱鸡?中二病患者?<br>口琴爱好者?',
+    '也是求知的弱鸡.中二病患者.<br>口琴不会吹.',
+    '也是求知的弱鸡.中二病患者.<br>口琴不会吹.',
+    '也是求知的弱鸡.中二病患者.<br>根本不会吹.',
+    '也是求知的弱鸡.中二病患者.<br>根本不会吹.',
+    '也是求知的弱鸡.中二病患者.<br>根本不会吹.',
+
+]
 var Change1 = function(type, id) {
     var opacityNum = 'translateY(-' + type + '%)';
     var scaleY = 'scaleY(' + (1 + (type / 3)) + ')';
@@ -27,26 +46,28 @@ var Change1 = function(type, id) {
     // console.log(opacityNum);
     $('#' + id).css('transform', opacityNum);
     $('#DS_line').css({ 'transform': scaleY, 'background-color': bgcolor });
-
     $("#DS_title_one").html(title1[textlimut]);
+    $("#DS_describe_one").html(describe[textlimut]);
+
+
 }
 
 window.onscroll = function(e) {
     var Dawn = document.documentElement.scrollTop || document.body.scrollTop;
-    // console.log(Dawn);
+    console.log(Dawn);
     var Progress;
     if (Dawn >= 0 && Dawn < 5) {
         $('#Ds_watch_more').css('display', 'block');
     } else {
         $('#Ds_watch_more').css('display', 'none');
     }
-    if (Dawn > 0 && Dawn < 500) {
+    if (Dawn > 0 && Dawn < 1000) {
         $('#DS_div_one').addClass("active");
-        Progress = parseInt((Dawn - 0) / 5);
-        console.log('%' + Progress);
+        Progress = parseInt((Dawn - 0) / 15);
+        // console.log('%' + Progress);
         Change1(Progress, "DS_div_one");
     }
-    if (Dawn > 500 && Dawn < 1000) {
+    if (Dawn > 1000 && Dawn < 2000) {
         $('#DS_div_one').removeClass("active");
 
     }
